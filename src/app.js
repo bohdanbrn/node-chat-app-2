09d31b5783d92615a7f29b911a02c77ed0e72a5f
bohdanbrn <bohdanbrn@gmail.com>
2019-03-17 22:51:27 +0200

    set up routes

--- src/app.js
+++ src/app.js
@@ -1,13 +1,35 @@
 const path = require('path');
 const express = require('express');
+const mustacheExpress = require('mustache-express');
+const mongoose = require('mongoose');
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+// DB config
+const db = require('./config/keys').MongoURI;
+
+// Connect to Mongo
+mongoose.connect(db, {useNewUrlParser: true})
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
+
 const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, () => {
-    console.log(`Server is run on port ${port}!`);
+// Register '.mustache' extension with The Mustache Express
+app.engine('mustache', mustacheExpress());
+
+// Mustache
+app.set('view engine', 'mustache');
+app.set('views', path.join(__dirname, '/views'));
+
+// Routes
+app.use('/', require('./routes/index'));
+app.use('/', require('./routes/login'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is run on port ${PORT}!`);
 })
