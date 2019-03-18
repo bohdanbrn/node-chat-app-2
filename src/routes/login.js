989176dc689661c8e3528f5a22f7ea6d955bf41e
bohdanbrn <bohdanbrn@gmail.com>
2019-03-18 23:48:59 +0200

    User authentication

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const passport = require('passport');
 
+// TODO (maybe bug User => user)
 // User model
-const User = require('../models/user');
+const User = require('../models/User');
 
 // Login Page
 router.get('/login', (req, res) => res.render('login'));
@@ -92,6 +94,22 @@ router.post('/register', (req, res) => {
                 }
             });
     }
+});
+
+// Login Handle
+router.post('/login', (req, res, next) => {
+    passport.authenticate('local', {
+        successRedirect: '/dashboard',
+        failureRedirect: '/login',
+        failureFlash: true
+    })(req, res, next);
+});
+
+// Logout
+router.get('/logout', (req, res) => {
+    req.logout();
+    req.flash('success_msg', 'You are logged out');
+    res.redirect('/login');
 });
 
 module.exports = router;
